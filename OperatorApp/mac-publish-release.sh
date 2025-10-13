7d732d1fe55f1beef37a2b15d1323e4e6b1a912d
Julik <julik@Yuris-MacBook-Pro.local>
2025-10-13 15:56:50 +0300

    Adding publish script for OperatorApp in Mac to contain also x64 CPU configuration. Debug compilation is not touched.

--- OperatorApp/mac-publish-release.sh
+++ OperatorApp/mac-publish-release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Running after build by while the server is running locally (not AWS) by
-# OperatorApp> ./Publish/MacHostApp localhost
+# OperatorApp> ./Publish_x64/MacHostApp localhost 5000 
+# OperatorApp> ./Publish_arm64/MacHostApp localhost 5000
 
 # Running after build while the server is running at AWS by
-# OperatorApp> ./Publish/MacHostApp <ElasticIP>
+# OperatorApp> ./Publish_x64/MacHostApp <ElasticIP> 80
+# OperatorApp> ./Publish_arm64/MacHostApp <ElasticIP> 80
 
 # Prevent Angular CLI from prompting
 export NG_CLI_ANALYTICS=ci
@@ -26,14 +28,18 @@ echo "[3/5] Publishing Backend (Release)..."
 dotnet publish Backend/Backend.csproj -c Release -r osx-arm64 --self-contained -o TempPublish/Backend || exit 1
 
 echo "[4/5] Publishing MacHostApp (Release)..."
-dotnet publish MacHostApp/MacHostApp.csproj -c Release -r osx-arm64 --self-contained -o Publish || exit 1
+dotnet publish MacHostApp/MacHostApp.csproj -c Release -r osx-arm64 --self-contained -o Publish_arm64 || exit 1
+dotnet publish MacHostApp/MacHostApp.csproj -c Release -r osx-x64 --self-contained -o Publish_x64 || exit 1
 
-echo "[5/5] Moving Backend into Publish\Backend..."
-mkdir -p Publish/Backend || exit 1
-cp -R TempPublish/Backend/* Publish/Backend/ || exit 1
+
+echo "[5/5] Copying Backend into Publish_arm64\Backend and into Publish_x64\Backend ..."
+mkdir -p Publish_arm64/Backend || exit 1
+cp -R TempPublish/Backend/* Publish_arm64/Backend/ || exit 1
+mkdir -p Publish_x64/Backend || exit 1
+cp -R TempPublish/Backend/* Publish_x64/Backend/ || exit 1
 
 echo "Cleaning up temporary Backend publish folder..."
 rm -rf TempPublish || exit 1
 
 echo "Publish complete! Ready to distribute:"
-echo "Publish/MacHostApp"
+echo "Publish_arm64/MacHostApp and Publish_x64/MacHostApp"
