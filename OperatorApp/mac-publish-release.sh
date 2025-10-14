2496375c7a5263e4d56e4f34dcc5f1af5fdc9ea8
Julik <julik@Yuris-MacBook-Pro.local>
2025-10-14 20:47:03 +0300

    OperatorAppp - mac - updating publish Backend for x64 also.

--- OperatorApp/mac-publish-release.sh
+++ OperatorApp/mac-publish-release.sh
@@ -11,7 +11,7 @@
 # Prevent Angular CLI from prompting
 export NG_CLI_ANALYTICS=ci
 
-echo "[1/5] Building Angular (production)..."
+echo "[1/7] Building Angular (production)..."
 
 cd frontend
 npm install --no-audit --silent || exit 1
@@ -19,26 +19,28 @@ ng build --configuration production || exit 1
 
 cd ..
 
-echo "[2/5] Copying Angular build to Backend\wwwroot..."
+echo "[2/7] Copying Angular build to Backend\wwwroot..."
 rm -rf Backend/wwwroot || exit 1
 mkdir -p Backend/wwwroot || exit 1
 cp -R frontend/dist/frontend/browser/* Backend/wwwroot/ || exit 1
 
-echo "[3/5] Publishing Backend (Release)..."
-dotnet publish Backend/Backend.csproj -c Release -r osx-arm64 --self-contained -o TempPublish/Backend || exit 1
+echo "[3/7] Publishing Backend (Release)..."
+dotnet publish Backend/Backend.csproj -c Release -r osx-arm64 --self-contained -o TempPublish/Backend_arm64 || exit 1
+dotnet publish Backend/Backend.csproj -c Release -r osx-x64 --self-contained -o TempPublish/Backend_x64 || exit 1
 
-echo "[4/5] Publishing MacHostApp (Release)..."
+echo "[4/7] Publishing MacHostApp (Release)..."
 dotnet publish MacHostApp/MacHostApp.csproj -c Release -r osx-arm64 --self-contained -o Publish_arm64 || exit 1
 dotnet publish MacHostApp/MacHostApp.csproj -c Release -r osx-x64 --self-contained -o Publish_x64 || exit 1
 
-
-echo "[5/5] Copying Backend into Publish_arm64\Backend and into Publish_x64\Backend ..."
+echo "[5/7] Copying Backend_arm64 into Publish_arm64\Backend ..."
 mkdir -p Publish_arm64/Backend || exit 1
-cp -R TempPublish/Backend/* Publish_arm64/Backend/ || exit 1
-mkdir -p Publish_x64/Backend || exit 1
-cp -R TempPublish/Backend/* Publish_x64/Backend/ || exit 1
+cp -R TempPublish/Backend_arm64/* Publish_arm64/Backend/ || exit 1
 
-echo "Cleaning up temporary Backend publish folder..."
+echo "[6/7] Copying Backend_x64 into Publish_x64\Backend ..."
+mkdir -p Publish_x64/Backend || exit 1
+cp -R TempPublish/Backend_x64/* Publish_x64/Backend/ || exit 1
+
+echo "[7/7] Cleaning up temporary Backend publish folder..."
 rm -rf TempPublish || exit 1
 
 echo "Publish complete! Ready to distribute:"
